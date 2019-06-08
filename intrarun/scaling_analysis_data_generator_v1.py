382b1be744b312c84f26df3d64a849a31daf916d
riddhisw <riddhi.sw@gmail.com>
2019-06-08 23:08:53 +1000

    Scaling analysis needs to be redone for the correct distance measure - namely the marginal weights and the posterior sstate

---
+++ intrarun/scaling_analysis_data_generator_v1.py
@@ -0,0 +1,141 @@
+import sys
+import os
+import copy
+import traceback
+import numpy as np
+
+########################
+# Find qslam modules
+########################
+sys.path.append('../qslam')
+
+from singlerun_v2 import SingleRunAnalysis2
+from riskanalysis import EngineeredTruth
+from qslamdesignparams import GLOBALDICT
+
+########################
+# Taking in bash parameters
+########################
+idx_prefix = int(sys.argv[1]) # truth flag type (three options)
+idx_job_array = int(sys.argv[2]) # job array starts from 1, represents repetitions
+idx_1 = int(sys.argv[3])  # index for msmt pair
+idx_2 = int(sys.argv[4])  # index for lambda pair
+
+########################
+# Truth Parameters
+########################
+
+# Choose defaults to match floor case (heights didn't work)
+TRUTHKWARGS = {}
+
+BARRIER_FLOOR = 0.25*np.pi
+BARRIER_HEIGHT = 0.75*np.pi
+FLOOR_RATIO = 0.4 # matches floor case
+
+TRUTHKWARGS["OneStepdheight"] = {"low": BARRIER_FLOOR, 
+                                 "high": BARRIER_HEIGHT} 
+TRUTHKWARGS["OneStepdfloorarea"] = FLOOR_RATIO 
+
+
+########################
+# Save to path 
+########################
+
+path = '/scratch/QCL_KF/qslamdatapaper_v2/' # on Artemis
+#path = './data_v2/'
+
+########################
+# Set true field
+########################
+
+prefix_list = ['2019_Feb_1D', '2019_Feb_2D', '2019_Feb_2D_Gssn']
+prefix = prefix_list[idx_prefix]
+
+if idx_prefix == 0:
+    change_gridconfig = True # 1D
+    TRUTHFLAG = None # use TRUTHKWARGS
+    TRUTHKWARGS["truthtype"] = 'OneStepd' 
+
+if idx_prefix == 1:
+    change_gridconfig = False # 2D
+    TRUTHFLAG = None # use TRUTHKWARGS
+    TRUTHKWARGS["truthtype"] = 'OneStepq' 
+
+if idx_prefix == 2:
+    change_gridconfig = False # 2D
+    TRUTHFLAG = None # use TRUTHKWARGS
+    TRUTHKWARGS["truthtype"] = 'Gaussian' 
+
+########################
+# Set 1D Hardware if req
+########################
+
+if change_gridconfig is True:
+    
+    GLOBALDICT["GRIDDICT"] = {}
+    
+    for idx_posy in range(25):
+        
+        if idx_posy < 10 :
+            
+            GLOBALDICT["GRIDDICT"]["QUBIT_0" + str(idx_posy)] = (0.0, float(idx_posy))
+            
+        if idx_posy >= 10 :
+            
+            GLOBALDICT["GRIDDICT"]["QUBIT_" + str(idx_posy)] = (0.0, float(idx_posy))
+
+
+########################
+# Set Defaults
+########################
+
+GLOBALDICT["MODELDESIGN"]["MAX_NUM_ITERATIONS"] = 75
+GLOBALDICT["MODELDESIGN"]["MSMTS_PER_NODE"] = 1
+GLOBALDICT["MODELDESIGN"]["MULTIPLER_R_MAX"] = 4.
+
+
+
+numofnodes=25
+particleconfigs = [ [3,2], [9,6], [15,10], [21,14], [30, 20]]
+
+prefix = '_idx_prefix_'+str(idx_prefix)+'_'
+fname_likelihood = 'scaling_'+str(idx_1)+'_'+str(idx_2)+'_'
+lambda_paris_2 = np.load('lambda_pairs_2.npz')
+random_variances = np.load('random_variances.npz')
+
+IDX1_SHP = len(random_variances['g1var'])
+IDX2_SHP = 30 # len(lambda_paris_2['lambda_1']) not doing all 250
+IDX3_SHP = len(particleconfigs)
+
+
+
+########################
+# Run Script
+########################
+
+truemap_generator = EngineeredTruth(numofnodes, TRUTHKWARGS)
+true_map_ = truemap_generator.get_map()
+
+GLOBALDICT["NOISEPARAMS"]["SIGMOID_APPROX_ERROR"]["SIGMA"] = random_variances['g2var'][idx_1]
+GLOBALDICT["NOISEPARAMS"]["QUANTISATION_UNCERTY"]["SIGMA"] = random_variances['g1var'][idx_1]
+GLOBALDICT["MODELDESIGN"]["LAMBDA_1"] = lambda_paris_2['lambda_1'][idx_2]
+GLOBALDICT["MODELDESIGN"]["LAMBDA_2"] = lambda_paris_2['lambda_2'][idx_2]
+
+
+
+
+for idx_3 in range(IDX3_SHP):
+
+    GLOBALDICT["MODELDESIGN"]["P_ALPHA"] = particleconfigs[idx_3][0]
+    GLOBALDICT["MODELDESIGN"]["P_BETA"] = particleconfigs[idx_3][1]
+
+
+    SAMPLE_GLOBAL_MODEL = copy.deepcopy(GLOBALDICT)
+
+
+    uniform_r_expt = SingleRunAnalysis(SAMPLE_GLOBAL_MODEL, true_map_, beta_expansion_mode=False)
+    uniform_r_expt.run_analysis(path+'Uni_R'+prefix+fname_likelihood+str(idx_3)+'_'+idx_job_array)
+
+    trunc_r_expt = SingleRunAnalysis(SAMPLE_GLOBAL_MODEL, true_map_, beta_expansion_mode=True)
+    trunc_r_expt.run_analysis(path+'Trunc_R'+prefix+fname_likelihood+str(idx_3)+'_'+idx_job_array)
+
---
+++ qslam/singlerun_v2.py
@@ -0,0 +1,123 @@
+import qslamr as qs
+import numpy as np
+
+class SingleRunAnalysis2(object):
+    
+    def __init__(self, 
+                 SAMPLE_GLOBAL_MODEL,true_map_,repts=1,
+                 measurements_controls_= None,
+                 autocontrol_="ON",
+                 var_thres_=1.0,
+                 numofnodes =25,
+                 beta_expansion_mode=False,
+                 beta_skew_adjust=False):
+
+        self.SAMPLE_GLOBAL_MODEL = SAMPLE_GLOBAL_MODEL
+        self.true_map_ = true_map_
+        self.repts = repts
+        self.measurements_controls_=measurements_controls_
+        self.autocontrol_=autocontrol_
+        self.var_thres_=var_thres_
+        self.numofnodes=numofnodes
+        self.beta_expansion_mode=beta_expansion_mode
+        self.beta_skew_adjust=beta_skew_adjust
+        
+    def call_qslam(self):
+        
+        qslamobj=0
+        qslamobj = qs.ParticleFilter(self.SAMPLE_GLOBAL_MODEL, 
+                                     save_run=True, 
+                                     beta_expansion_mode=self.beta_expansion_mode, 
+                                     beta_skew_adjust=self.beta_skew_adjust)
+
+        qslamobj.QubitGrid.engineeredtruemap = self.true_map_
+
+        qslamobj.qslamr(measurements_controls=self.measurements_controls_,
+                        autocontrol=self.autocontrol_,
+                        max_num_iterations=self.SAMPLE_GLOBAL_MODEL["MODELDESIGN"]["MAX_NUM_ITERATIONS"],
+                        var_thres=self.var_thres_)
+
+        return qslamobj
+
+    def run_analysis(self, filename='./SingleRunAnalysis'):
+    
+        max_num_iterations=self.SAMPLE_GLOBAL_MODEL["MODELDESIGN"]["MAX_NUM_ITERATIONS"]
+
+        # --------------------------------------------------------------------
+        # Make true error matrix and rate of change matrix for a single run
+        # --------------------------------------------------------------------
+
+        absolute_errors_matrix = np.zeros((max_num_iterations, self.numofnodes ))
+        rate_of_change_of_map = np.zeros((max_num_iterations, self.numofnodes ))
+        rate_of_change_of_len = np.zeros((max_num_iterations, self.numofnodes ))
+
+        qslamobj = self.call_qslam()
+
+        for idxt in range(0, max_num_iterations): 
+
+            f_strt = self.numofnodes * 2  # fixed by QSLAM design of state vector
+            f_end = self.numofnodes * 3
+            r_strt = self.numofnodes * 3
+
+            absolute_errors_matrix[idxt, :]  = abs(qslamobj.save_posterior_state[idxt][f_strt:f_end] - self.true_map_)
+
+            if idxt > 0:
+
+                rate_of_change_of_map[idxt, :] = abs(qslamobj.save_posterior_state[idxt][f_strt:f_end] - qslamobj.save_posterior_state[idxt-1][f_strt:f_end])
+                rate_of_change_of_len[idxt, :] = abs(qslamobj.save_posterior_state[idxt][r_strt:] - qslamobj.save_posterior_state[idxt-1][r_strt:])
+
+        # --------------------------------------------------------------------
+        # Make matrices for distribution of weights for the last repetition
+        # --------------------------------------------------------------------
+
+
+        P_ALPHA = self.SAMPLE_GLOBAL_MODEL["MODELDESIGN"]["P_ALPHA"]
+        P_BETA = self.SAMPLE_GLOBAL_MODEL["MODELDESIGN"]["P_BETA"]
+
+        predictive_weights = np.zeros((max_num_iterations, P_ALPHA))
+        posterior_weights = np.zeros((max_num_iterations, P_ALPHA))
+        leaf_weights = np.zeros((max_num_iterations, P_ALPHA, P_BETA))
+        joint_weights = np.zeros((max_num_iterations, P_ALPHA * P_BETA)) 
+
+        marginalised_weights = np.zeros((max_num_iterations, P_ALPHA))
+
+
+        alpha_labels = range(1, P_ALPHA + 1, 1)
+        beta_labels = range(1, P_BETA + 1, 1)
+        joint_labels = range(1, P_ALPHA*P_BETA + 1, 1)
+
+        for idxt in range(0, max_num_iterations): 
+
+            predictive_weights[idxt, :] = np.asarray([qslamobj.save_alpha_predictive[idxt][idx].weight for idx in range(P_ALPHA)])
+
+            start = P_ALPHA * idxt
+            end = start + P_ALPHA
+            leaf_weights[idxt, :, :] = np.asarray(qslamobj.save_all_beta_weights[start :  end])
+
+            joint_weights[idxt, :] = qslamobj.save_alpha_beta_joint_weights[idxt]
+
+            marginalised_labels = qslamobj.save_alpha_bar_2_oldnodes[idxt]
+
+            for idx_m in range(len(marginalised_labels)):
+                idx_particle = marginalised_labels[idx_m]
+                marginalised_weights[idxt, idx_particle] = np.asarray(qslamobj.save_alpha_bar_2_weights[idxt])[idx_m]
+
+            posterior_weights[idxt, :] = np.asarray([qslamobj.save_alpha_posterior[idxt][idx].weight for idx in range(P_ALPHA)])
+            
+        
+        np.savez(filename,
+                 alpha_labels=alpha_labels, 
+                 beta_labels=beta_labels, 
+                 joint_labels=joint_labels, 
+                 absolute_errors_matrix=absolute_errors_matrix, 
+                 rate_of_change_of_map=rate_of_change_of_map, 
+                 rate_of_change_of_len=rate_of_change_of_len, 
+                 predictive_weights=predictive_weights, 
+                 leaf_weights=leaf_weights, 
+                 joint_weights=joint_weights, 
+                 marginalised_weights=marginalised_weights, 
+                 posterior_weights=posterior_weights)
+       
+        return
+    
+    
