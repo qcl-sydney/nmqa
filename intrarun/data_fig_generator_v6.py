49c6755d9f38c93d57a77c8bbc956a32add5622e
riddhisw <riddhi.sw@gmail.com>
2019-07-22 14:52:55 +1000

    Technical long paper draft figures generation; also marks codebase at end of NPJQI rebuttal

--- intrarun/data_fig_generator_v6.py
+++ intrarun/data_fig_generator_v6.py
@@ -64,7 +64,7 @@ for idx_truth_flag in range(5):
         SIMULATIONSDICT[idx_truth_flag][idx_expandtype]["zerolambda"]["idx_1"] = None
         SIMULATIONSDICT[idx_truth_flag][idx_expandtype]["zerolambda"]["idx_2"] = 0 
 
-    SIMULATIONSDICT[idx_truth_flag]["GRAPHMIN"] = -2.5 # Figure plotting
+    SIMULATIONSDICT[idx_truth_flag]["GRAPHMIN"] = -2.5# -1.9 # Figure plotting
     SIMULATIONSDICT[idx_truth_flag]["GRAPHMAX"] = 0.7 #1 # Figure plotting
     SIMULATIONSDICT[idx_truth_flag]["R_MAX"] = 150 # Figure plotting
     SIMULATIONSDICT[idx_truth_flag]["F_MAX"] = 8.5 # Figure plotting
@@ -76,8 +76,8 @@ SIMULATIONSDICT[3]["num_of_nodes"] = 9
 SIMULATIONSDICT[4]["num_of_nodes"] = 16
 
 SIMULATIONSDICT[0]["R_MAX"] = 300 # Figure plotting
-SIMULATIONSDICT[1]["R_MAX"] = 75 # Figure plotting
-SIMULATIONSDICT[2]["R_MAX"] = 75 # Figure plotting
+SIMULATIONSDICT[1]["R_MAX"] = 80 # Figure plotting
+SIMULATIONSDICT[2]["R_MAX"] = 80 # Figure plotting
 SIMULATIONSDICT[3]["R_MAX"] = 20 # Figure plotting
 SIMULATIONSDICT[4]["R_MAX"] = 75 # Figure plotting
 
@@ -182,31 +182,28 @@ for idx_figure in range(2):
         error_scaling_matrix_1[idx_pconfig, :] = np.mean(np.mean(test_data_1['absolute_errors_matrix']**2, axis=0), axis=1)
 
     particle_number = np.asarray([idp[0] for idp in particleconfigs] ) 
-    idx_pconfig_T = np.argmin(error_scaling_matrix_1[:,74])
-    idx_pconfig_U = np.argmin(error_scaling_matrix_0[:,74])
+    
+    pick_t_regime = 74
+    idx_pconfig_T = np.argmin(error_scaling_matrix_1[:, pick_t_regime])
+    idx_pconfig_U = np.argmin(error_scaling_matrix_0[:, pick_t_regime])
     
     if BASELINE:
         
         # Only Uniform expansion used in baseline:
-        # baseline_0['absolute_errors_matrix'].shape is (50, 75, 25) (repts, iter, grid)
+        # baseline_0['absolute_errors_matrix'].shape is (50, SIMULATIONSDICT[idx_truth_flag]["max_iterations"], 25) (repts, iter, grid)
         baseline_error_scaling_0 = 100.*np.ones((1, max_iterations))
         baseline_0 = np.load(path+datatype[0]+prefix+'baseline_.npz')
         baseline_error_scaling_0[0, :] = np.mean(np.mean(baseline_0['absolute_errors_matrix']**2, axis=0), axis=1)
         
     #################################
-    # FIGURE 1
+    # FIGURE : expansion behaviour
     #################################
 
-    colorsU = [ 'indianred', 'orangered', 'salmon', 'palevioletred' ]#, 'pink']
-    colors = ['midnightblue', 'slateblue',   'steelblue', 'gray']#'lightsteelblue']
-    colors=colors[::-1]
-    colorsU=colorsU[::-1]
-    times =[1, 20, 40 ,74]
+    gslayout = gs(10,1, top = 0.99, bottom =0.05, left = 0.15, right = 0.98, wspace = 0.05, hspace = 0.4) # MAIN TEXT
+    # gslayout = gs(1, 1, top = 0.99, bottom =0.1, left = 0.1, right = 0.98, wspace = 1.0, hspace = 0.01)  # APPENDIX
 
-    gslayout = gs(2,2, top = 0.99, bottom =0.1, left = 0.15, right = 0.98, wspace = 0.05, hspace = 0.2)
-
-    fig = plt.figure(figsize=(cm2inch(6.),cm2inch(12)))
-
+    fig = plt.figure(figsize=(cm2inch(6.),cm2inch(8))) # MAIN TEXT
+    # fig = plt.figure(figsize=(cm2inch(7.8),cm2inch(4.5))) # APPENDIX 
 
     plt.suptitle('TRUNC (g1, g2) = (%s, %s) and (l1, l2) = (%s, %s) UNI (g1, g2) = (%s, %s) and (l1, l2) = (%s, %s)' %(likelihoodparams_scan['g1var'][idx_1], 
                                                                  likelihoodparams_scan['g2var'][idx_1],
@@ -218,67 +215,27 @@ for idx_figure in range(2):
                                                                  lambda_pairs['lambda_2'][idx_2_U]),
                 )
 
-    ax_err = fig.add_subplot(gslayout[0, :])
-    ax_scl = fig.add_subplot(gslayout[1, 1])
-    ax_uni = fig.add_subplot(gslayout[1, 0])
-
-    #################################
-    # bottom: scaling behaviour
-    #################################
-    for idx_t in range(len(times)):
-
-        slope, intercept = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_1[:, times[idx_t]]), 1)
-
-        ax_scl.plot(np.log(particle_number), np.log( error_scaling_matrix_1[:, times[idx_t]]), 
-                 'o', ms=4, markerfacecolor='None', lw=1., c= colors[idx_t], label= r'$t=%s, \Delta = %s$'%(times[idx_t], np.round(slope, 2)))
-
-        trendpoly = np.poly1d([slope,intercept]) 
-
-        ax_scl.plot(np.log(particle_number),trendpoly(np.log(particle_number)), '--',  lw=1., c= colors[idx_t])
-        ax_scl.axvline(x=np.log(particleconfigs[idx_pconfig_T][0]), ls='-', lw=1., c='steelblue', alpha=0.15)
-
-    # ax_scl.set_xlabel(r'$\log(n_\alpha)$  (a.u.)')
-    # ax_scl.set_ylabel(r'$\log(\mathbb{E}[|\pi^n_t - \pi_\infty|])$  (a.u.)')
-    ax_scl.set_ylim([GRAPHMIN, GRAPHMAX])
-    ax_scl.legend(loc=1)
-    ax_scl.yaxis.set_ticklabels([])
-
-
-
-    for idx_t in range(len(times)):
-
-        slope, intercept = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_0[:, times[idx_t]]), 1)
-
-        ax_uni.plot(np.log(particle_number), np.log( error_scaling_matrix_0[:, times[idx_t]]), 
-                 'x', lw=1., ms=4, c= colorsU[idx_t], label= r'$t=%s, \Delta = %s$'%(times[idx_t], np.round(slope, 2)))
-
-        trendpoly = np.poly1d([slope,intercept]) 
-
-        ax_uni.plot(np.log(particle_number),trendpoly(np.log(particle_number)), '--', lw=1.,  c= colorsU[idx_t])
-        ax_uni.axvline(x=np.log(particleconfigs[idx_pconfig_U][0]), ls='-', lw=1., c='indianred', alpha=0.15)
-
-    # ax_uni.set_xlabel(r'$\log(n_\alpha)$  (a.u.)')
-    # ax_uni.set_ylabel(r'$\log(\mathbb{E}[|\pi^n_t - \pi_\infty|])$  (a.u.)')
-    ax_uni.set_ylim([GRAPHMIN, GRAPHMAX])
-    ax_uni.legend(loc=1)
-
+    ax_err = fig.add_subplot(gslayout[0:7, 0])  # MAIN TEXT
+    # ax_err = fig.add_subplot(gslayout[0, 0])  # APPENDIX
+    ax1 = fig.add_subplot(gslayout[7:, 0]) # MAIN TEXT
+    
     #################################
     # top: error trajectory
     #################################
     
     if BASELINE:
-        ax_err.plot(range(75)[::2],np.log(baseline_error_scaling_0[0, ::2]), '-', 
+        ax_err.plot(range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"])[::2],np.log(baseline_error_scaling_0[0, ::2]), '-', 
                     lw=1., ms=4, c='k', 
                     label='Baseline')
         
 
-    ax_err.plot(range(75)[::2],np.log(error_scaling_matrix_0[idx_pconfig_U, ::2]), 'x--', 
+    ax_err.plot(range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"])[::2],np.log(error_scaling_matrix_0[idx_pconfig_U, ::2]), 'x--', 
                 lw=1., ms=4, c='indianred', 
                 label='Uniform')
 
 
 
-    ax_err.plot(range(75)[::2],np.log(error_scaling_matrix_1[idx_pconfig_T, ::2]), 'o--', 
+    ax_err.plot(range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"])[::2],np.log(error_scaling_matrix_1[idx_pconfig_T, ::2]), 'o--', 
                 lw=1., ms=4, 
                 c='steelblue',
                 markerfacecolor='None', label='Trunc. Gauss', alpha=1.)
@@ -288,50 +245,109 @@ for idx_figure in range(2):
     ax_err.set_ylim([GRAPHMIN, GRAPHMAX])
     ax_err.axvline(x=num_of_nodes, ls='-', color='grey', linewidth=0.5, label='Num. of qubits')
     ax_err.legend(loc=0)
-
-    plt.savefig(savetopath+'fig_'+str(idx_paramtype)+'_error_OPT_'+str(idx_prefix)+'.svg', format='svg', dpi=800)
-    plt.show()
-
+    ax_err.xaxis.set_ticklabels([]) # MAIN TEXT
+    
     #################################
-    # FIGURE 2: Insets of rate of change of R and F
+    # bottom: Rate of change of R
     #################################
 
     test_data_0 = np.load(path+datatype[0]+prefix+'rand_'+str(idx_1_U)+'_'+str(idx_2_U)+'_'+str(idx_pconfig_T)+'.npz')
     test_data_1 = np.load(path+datatype[1]+prefix+'rand_'+str(idx_1)+'_'+str(idx_2)+'_'+str(idx_pconfig_T)+'.npz')
-
-    rate_of_change_of_map_0 = test_data_0['rate_of_change_of_map'][np.random.randint(low=0, high=50)]
     rate_of_change_of_len_0 = test_data_0['rate_of_change_of_len'][np.random.randint(low=0, high=50)]
-    rate_of_change_of_map_1 = test_data_1['rate_of_change_of_map'][np.random.randint(low=0, high=50)]
     rate_of_change_of_len_1 = test_data_1['rate_of_change_of_len'][np.random.randint(low=0, high=50)]
-
-    fig = plt.figure(figsize=(cm2inch(6.),cm2inch(6.)))
-    gslayout = gs(2,2, top = 0.99, bottom =0.1, left = 0.15, right = 0.98, wspace = 0.1, hspace = 0.2)
-
-    ax3 = fig.add_subplot(gslayout[1, 0])
-    ax3.set_ylabel(r"$ | \hat{F}_t - \hat{F}_{t-1}|$  (rad)")
-    ax3.plot(np.sum(rate_of_change_of_map_0, axis=1)[1:], c='indianred',lw=1)
-    ax3.set_ylim([0., F_MAX])
-
-
-    ax1 = fig.add_subplot(gslayout[0, 0])
-    ax1.set_ylabel(r"$ | \hat{R}_t - \hat{R}_{t-1}|$  (dist)")
-    ax1.plot(np.sum(rate_of_change_of_len_0, axis=1)[1:], c='indianred', lw=1, label='Uniform')
-    ax1.set_ylim([0.00, R_MAX])
+    
+    # MAIN TEXT
+    ax1.plot(np.sum(rate_of_change_of_len_0, axis=1)[1:], ls='--', c='indianred', lw=1.1, label='Uniform')
+    ax1.plot(np.sum(rate_of_change_of_len_1, axis=1)[1:], ls='-', c='steelblue', lw=1.2, label='Trunc. Gauss')
+    ax1.set_ylim([-0.1, R_MAX])
     ax1.legend(loc=0)
-    #plt.yscale('log')
-
-
-    ax4 = fig.add_subplot(gslayout[1,1])
-    ax4.plot(np.sum(rate_of_change_of_map_1, axis=1)[1:], c='steelblue', lw=1)
-    ax4.set_ylim([0., F_MAX])
-    ax4.set_xlabel(r'$t$  (num)')
-    ax4.yaxis.set_ticklabels([])
-
-    ax2 = fig.add_subplot(gslayout[0,1])
-    ax2.plot(np.sum(rate_of_change_of_len_1, axis=1)[1:], c='steelblue', lw=1, label='Trunc. Gauss')
-    ax2.set_ylim([0.0, R_MAX])
-    ax2.legend(loc=0)
-    ax2.yaxis.set_ticklabels([])
-
-    plt.savefig(savetopath+'fig_'+str(idx_paramtype)+'_error_state_change_OPT_'+str(idx_prefix)+'.svg', format='svg', dpi=800)
+    
+    plt.savefig(savetopath+'fig_'+str(idx_paramtype)+'_expansion_v6_'+str(idx_prefix)+'.svg', format='svg', dpi=800)
     plt.show()
+    #################################
+    # FIGURE: Scaling Behaviour
+    #################################
+    
+    fig = plt.figure(figsize=(cm2inch(7.8),cm2inch(4.5))) # MAIN TEXT # APPENDIX
+    # fig = plt.figure(figsize=(cm2inch(3.5),cm2inch(3.))) # ZEROLAMBDA
+    
+    gslayout = gs(1, 1, top = 0.99, bottom =0.1, left = 0.1, right = 0.98, wspace = 1.0, hspace = 0.01)
+    
+    #################################
+    # left: Delta vs. t behaviour
+    #################################
+    ax_scl = fig.add_subplot(gslayout[0, 0])
+    
+    delta_trace_0 = []
+    delta_trace_1 = []
+    
+    for idx_t in range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"]):
+
+        slope_0, intercept_0 = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_0[:, idx_t]), 1)
+        slope_1, intercept_1 = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_1[:, idx_t]), 1)
+        delta_trace_0.append(slope_0)
+        delta_trace_1.append(slope_1)
+    
+    delta_trace_0 = np.asarray(delta_trace_0)
+    delta_trace_1 = np.asarray(delta_trace_1)
+    ax_scl.plot(range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"])[::2], delta_trace_0[::2], 'x--', 
+                lw=1., ms=2, c='indianred', 
+                label='Uniform')
+
+    ax_scl.plot(range(SIMULATIONSDICT[idx_truth_flag]["max_iterations"])[::2], delta_trace_1[::2], 'o--', 
+                lw=1., ms=2, 
+                c='steelblue',
+                markerfacecolor='None', label='Trunc. Gauss', alpha=1.)
+    
+    ax_scl.set_ylim([-0.3, 0.75]) # MAIN TEXT
+    ax_scl.set_yticks(np.arange(-0.3, 0.76, 0.15)) # MAIN TEXT
+    # ax_scl.set_ylim([-0.3, 0.8]) # APPENDIX 
+    # ax_scl.set_yticks(np.arange(-0.3, 0.81, 0.15))   # APPENDIX
+    # ax_scl.set_ylim([-0.3, 0.45]) # ZEROLAMBDA 
+    # ax_scl.set_yticks(np.arange(-0.3, 0.451, 0.15))   # ZEROLAMBDA
+    ax_scl.set_xlim([0, 75])
+    ax_scl.axhline(y=0.0, ls='-', color='k', lw=0.5)
+    
+    
+    plt.savefig(savetopath+'fig_'+str(idx_paramtype)+'_scalingbehav_v6_'+str(idx_prefix)+'.svg', format='svg', dpi=800)
+    plt.show()
+    
+    #################################
+    # inset: Error scaling vs particle number and best fit at t=75
+    #################################
+    
+    fig = plt.figure(figsize=(cm2inch(2.75),cm2inch(1.9)))
+    gslayout = gs(1, 1, top = 0.99, bottom =0.1, left = 0.1, right = 0.98, wspace = 1.0, hspace = 0.01)
+    ax_inset = fig.add_subplot(gslayout[0, 0])
+    
+    
+    # Uniform
+    slope_0, intercept_0 = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_0[:, pick_t_regime]), 1)
+    ax_inset.plot(np.log(particle_number), np.log( error_scaling_matrix_0[:, pick_t_regime]), 
+                'x', ms=3.2, markerfacecolor='None', lw=1., c='indianred', label= 'Uniform')
+    trendpoly_0 = np.poly1d([slope_0,intercept_0]) 
+    ax_inset.plot(np.log(particle_number),trendpoly_0(np.log(particle_number)), '--',  lw=1., c='indianred', label= r'$\Delta = %s$'%(np.round(slope_0, 2)))
+    ax_inset.axvline(x=np.log(particleconfigs[idx_pconfig_U][0]), ls='-', lw=1., c='indianred', alpha=0.5)
+    
+    # Trunc Gaussian
+    slope_1, intercept_1 = np.polyfit(np.log(particle_number), np.log(error_scaling_matrix_1[:, pick_t_regime]), 1)
+    ax_inset.plot(np.log(particle_number), np.log( error_scaling_matrix_1[:, pick_t_regime]), 
+                'o', ms=3.2, markerfacecolor='None', lw=1., c='steelblue', label= 'Trunc. Gauss')
+    trendpoly_1 = np.poly1d([slope_1,intercept_1]) 
+    ax_inset.plot(np.log(particle_number),trendpoly_1(np.log(particle_number)), '--',  lw=1., c='steelblue', label= r'$\Delta = %s$'%(np.round(slope_1, 2)))
+    ax_inset.axvline(x=np.log(particleconfigs[idx_pconfig_T][0]), ls='-', lw=1., c='steelblue', alpha=0.5)
+    
+    # Both
+    ax_inset.set_ylim([-1.9, -1.2]) # Zoom into the t=75 case. # MAIN TEXT
+    ax_inset.set_yticks(np.arange(-1.9, -1.21, 0.3))  # MAIN TEXT
+    # ax_inset.set_ylim([-2.5, -1.]) # Zoom into the t=75 case. # APPENDIX #ZEROLAMBDA
+    # ax_inset.set_yticks(np.arange(-2.5, -1., 0.5)) # APPENDIX #ZEROLAMBDA
+    ax_inset.legend(loc=1, fontsize=8)
+    
+    # ax_inset.yaxis.set_ticklabels([])
+    
+    plt.savefig(savetopath+'fig_'+str(idx_paramtype)+'_scalinginset_v6_'+str(idx_prefix)+'.svg', format='svg', dpi=800)
+    plt.show()
+    
+
+
